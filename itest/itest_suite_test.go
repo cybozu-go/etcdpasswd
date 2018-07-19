73928732840c12cc1838715e54eaed70d2d2668d
ymmt <ymmt@cybozu.com>
2018-07-19 11:57:00 +0000

    [itest] run etcd, ep-agent for test.

--- itest/itest_suite_test.go
+++ itest/itest_suite_test.go
@@ -1,7 +1,8 @@
-package itest_test
+package itest
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -11,3 +12,24 @@ func TestItest(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Itest Suite")
 }
+
+var _ = BeforeSuite(func() {
+	err := prepareSshClients(host1, host2, host3)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = runEtcd(sshClients[host1])
+	Expect(err).NotTo(HaveOccurred())
+
+	time.Sleep(time.Second)
+
+	err = runEPAgent()
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = Describe("etcdpasswd is working", func() {
+	It("should not fail", func(done Done) {
+		_, _, err := runCommand(host1, "/data/etcdpasswd get start-uid")
+		Expect(err).NotTo(HaveOccurred())
+		close(done)
+	}, 10)
+})
--- itest/run.go
+++ itest/run.go
@@ -1,25 +1,16 @@
 package itest
 
 import (
-	"os"
-	"os/exec"
+	"bytes"
+	"errors"
 	"time"
 
 	"golang.org/x/crypto/ssh"
 )
 
-func runPlacemat() *exec.Cmd {
-	cmd := exec.Command(placemat, "cluster.yml")
-	if debug {
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-	}
-	err := cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-	return cmd
-}
+var (
+	sshClients = make(map[string]*ssh.Client)
+)
 
 func sshTo(address string) (*ssh.Client, error) {
 	config := &ssh.ClientConfig{
@@ -33,14 +24,13 @@ func sshTo(address string) (*ssh.Client, error) {
 	return ssh.Dial("tcp", address+":22", config)
 }
 
-func prepareSshClients(addresses ...string) map[string]*ssh.Client {
-	ret := make(map[string]*ssh.Client)
+func prepareSshClients(addresses ...string) error {
 	ch := time.After(time.Minute)
 	for _, a := range addresses {
 	RETRY:
 		select {
 		case <-ch:
-			panic("timed out")
+			return errors.New("timed out")
 		default:
 		}
 		client, err := sshTo(a)
@@ -48,11 +38,50 @@ func prepareSshClients(addresses ...string) map[string]*ssh.Client {
 			time.Sleep(time.Second)
 			goto RETRY
 		}
-		ret[a] = client
+		sshClients[a] = client
 	}
-	return ret
+	return nil
 }
 
 func runEtcd(client *ssh.Client) error {
+	command := "systemd-run --user /data/etcd --listen-client-urls=http://0.0.0.0:2379 --advertise-client-urls=http://localhost:2379"
+	sess, err := client.NewSession()
+	if err != nil {
+		return err
+	}
+	defer sess.Close()
+
+	return sess.Run(command)
+}
+
+func runEPAgent() error {
+	for _, c := range sshClients {
+		sess, err := c.NewSession()
+		if err != nil {
+			return err
+		}
+
+		err = sess.Run("sudo systemd-run /data/ep-agent")
+		sess.Close()
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func runCommand(host, command string) (stdout, stderr []byte, e error) {
+	client := sshClients[host]
+	sess, err := client.NewSession()
+	if err != nil {
+		return nil, nil, err
+	}
+	defer sess.Close()
+
+	outBuf := new(bytes.Buffer)
+	errBuf := new(bytes.Buffer)
+	sess.Stdout = outBuf
+	sess.Stderr = errBuf
+	err = sess.Run(command)
+	return outBuf.Bytes(), errBuf.Bytes(), err
 }
